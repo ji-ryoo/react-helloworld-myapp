0c8ba133aa9b00efb8daf69c813dcb8e44f9bff9
ji-ryoo <tremolorj09@gmail.com>
2021-03-20 16:21:34 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Welcome(props){
+  return <h1>Hello, {props.name}</h1>
+}
+
+function User(props){
+  return (
+    <h2>{props.myouji} {props.namae}</h2>
+  )
+}
+
+function UserInfo(props){
+  return (
+    <div>
+    <User myouji={props.m} namae={props.n}/>
+    <h3>あなたの好きな食べ物は{props.food}</h3>
+    </div>
+  )
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <Welcome name="gogh" />
+    <UserInfo m="ow" n="gogh" food="karaage"/>
+    <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
